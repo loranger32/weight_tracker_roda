b6cebdecbf32d63249890a4e441d41e82e098c79
loranger32 <laurentguinotte@gmail.com>
2021-03-13 16:46:47 +0100

    Add seed data for entries

--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -1,4 +1,12 @@
 require 'sequel'
 
-# DB = Sequel.connect(ENV['DATABASE_URL'])
+DB = Sequel.connect(ENV['DATABASE_URL'])
+
+entries = [ { weight: 85.0, day: "2021-01-01", note: 'First day' },
+            { weight: 84.0, day: "2021-01-02", note: '' },
+            { weight: 83.5, day: "2021-01-03", note: 'Great !' },
+            { weight: 83.2, day: "2021-01-04", note: 'Keep going' } ]
+
+entries.each { |entry| DB[:entries].insert(entry) }
+
 
