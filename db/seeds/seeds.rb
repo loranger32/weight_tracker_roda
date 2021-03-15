e5ba103578154ad3937e841b5d7a74c8fc44411d
loranger32 <laurentguinotte@gmail.com>
2021-03-15 11:55:53 +0100

    Format delta on entries_index view

--- db/seeds/seeds.rb
+++ db/seeds/seeds.rb
@@ -5,7 +5,8 @@ DB = Sequel.connect(ENV['DATABASE_URL'])
 entries = [ { weight: 85.0, day: "2021-01-01", note: 'First day' },
             { weight: 84.0, day: "2021-01-02", note: '' },
             { weight: 83.5, day: "2021-01-03", note: 'Great !' },
-            { weight: 83.2, day: "2021-01-04", note: 'Keep going' } ]
+            { weight: 83.2, day: "2021-01-04", note: 'Keep going' },
+            { weight: 84.0, day: "2021-01-05", note: "Don't give up"} ]
 
 entries.each { |entry| DB[:entries].insert(entry) }
 
--- helpers/view_helpers.rb
+++ helpers/view_helpers.rb
@@ -1,17 +1,21 @@
 module WeightTracker
-  class App
-    def weight_delta_color(delta)
-      if delta == 0
-        "blue"
-      elsif delta > 0
-        "red"
-      else
-        "green"
-      end
-    end
-
-    def create_or_update_entry_action(entry)
-      entry.new? ? "/entries" : "/entries/#{entry.id}"
+class App
+  def weight_delta_color(delta)
+    if delta == 0
+      "blue"
+    elsif delta > 0
+      "red"
+    else
+      "green"
     end
   end
+
+  def create_or_update_entry_action(entry)
+    entry.new? ? "/entries" : "/entries/#{entry.id}"
+  end
+
+  def format_delta(delta)
+    delta > 0 ? "+#{delta}" : delta.to_s
+  end
+end
 end
