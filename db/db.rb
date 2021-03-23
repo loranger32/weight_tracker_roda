ee8b4b85e24fc8cc1f85b95be12b582ab5450119
loranger32 <laurentguinotte@gmail.com>
2021-03-23 19:18:17 +0100

    Fix DB access in IRB console

--- db/db.rb
+++ db/db.rb
@@ -1,7 +1,7 @@
 require "sequel"
 require "logger"
 
-module MyApp
+module WeightTracker
   DB = if ENV["RACK_ENV"] == "test"
     Sequel.connect(ENV.fetch("TEST_DATABASE_URL"))
   else
@@ -13,3 +13,6 @@ module MyApp
   require_relative "../models/entry"
   require_relative "../models/account"
 end
+
+# In the irb console, save some typing when accessing the DB object
+DB = WeightTracker::DB
