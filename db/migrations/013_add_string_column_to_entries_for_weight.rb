7470e27bcbac3e9d69fbe8bc9ea061cce177d81d
loranger32 <laurentguinotte@gmail.com>
2021-04-29 12:03:51 +0200

    Remove null constraint on new string column

--- db/migrations/013_add_string_column_to_entries_for_weight.rb
+++ db/migrations/013_add_string_column_to_entries_for_weight.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
     alter_table(:entries) do
-      add_column :weight_string, String, size: 5, null: false
+      add_column :weight_string, String, size: 5
     end
   end
 end
