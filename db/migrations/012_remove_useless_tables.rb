f2f3b0c6ba7251461a4b0303f83146f6271eb61f
loranger32 <laurentguinotte@gmail.com>
2021-04-29 11:06:17 +0200

    Generate the down block of the last migration

--- db/migrations/012_remove_useless_tables.rb
+++ db/migrations/012_remove_useless_tables.rb
@@ -7,5 +7,32 @@ Sequel.migration do
   end
 
   down do
+    # Used by the webauthn feature
+    create_table(:account_webauthn_user_ids) do
+      foreign_key :id, :accounts, primary_key: true, type: :Bignum
+      String :webauthn_id, null: false
+    end
+
+    # Used by the webauthn feature
+    create_table(:account_webauthn_user_ids) do
+      foreign_key :id, :accounts, primary_key: true, type: :Bignum
+      String :webauthn_id, null: false
+    end
+
+    # Used by the sms codes feature
+    create_table(:account_sms_codes) do
+      foreign_key :id, :accounts, primary_key: true, type: :Bignum
+      String :phone_number, null: false
+      Integer :num_failures
+      String :code
+      DateTime :code_issued_at, null: false, default: Sequel::CURRENT_TIMESTAMP
+    end
+    
+    # Used by the remember me feature
+    create_table(:account_remember_keys) do
+      foreign_key :id, :accounts, primary_key: true, type: :Bignum
+      String :key, null: false
+      DateTime :deadline, deadline_opts[14]
+    end
   end
 end
