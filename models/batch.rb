a6b5bd358c2b771dd6d2ea2bac0e892130e46330
loranger32 <laurentguinotte@gmail.com>
2021-05-05 15:28:55 +0200

    Add validations for Batch model

--- models/batch.rb
+++ models/batch.rb
@@ -1,4 +1,15 @@
 class Batch < Sequel::Model
+  plugin :validation_helpers
+
   one_to_many :entries
   many_to_one :account
+
+  def validate
+    super
+    validates_presence [:account_id, :active]
+    validates_integer :account_id
+    # TO DO : active param is always evaluated in a boolean context, which means it's always true or false
+    # Validation should be more specific
+    validates_type [TrueClass, FalseClass], :active
+  end
 end
