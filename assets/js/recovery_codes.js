30b66c44477cb8fc09a2aa56cc73098304a048b7
loranger32 <laurentguinotte@gmail.com>
2022-04-10 21:55:54 +0200

    Add a Bootstrap 5 popover on the copy recovery codes button

--- assets/js/recovery_codes.js
+++ assets/js/recovery_codes.js
@@ -1,5 +1,18 @@
 "use strict"
 
+// Initializes Popovers - Bootstrap 5
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// TO FIX - Recommanded by Bootstrap 5 docs, ans it works, but generates an error
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+})
+
+
+// Page specific JS
 const printButton = document.querySelector("#print")
 const copyButton = document.querySelector("#copy")
 
@@ -15,6 +28,7 @@ function copy() {
   // Code snippet comes from https://htmldom.dev/copy-text-to-the-clipboard
 
   let recoveryCodes = document.querySelector("#recovery-codes")
+  console.log(recoveryCodes);
   let text = recoveryCodes.textContent;
 
   // Create a "fake" textarea
@@ -47,5 +61,3 @@ function copy() {
       document.body.removeChild(textAreaEle);
   }
 }
-
-
