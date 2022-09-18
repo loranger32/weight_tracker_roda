5615cc75b5afff0937a7fd76010587bcbc7fc021
loranger32 <laurentguinotte@gmail.com>
2022-09-18 16:00:00 +0200

    entries_actions_test - don't use model associations - cacing issues

--- test/integration/entries_actions_test.rb
+++ test/integration/entries_actions_test.rb
@@ -5,7 +5,7 @@ class EntriesActionTest < CapybaraTestCase
     super
     clean_test_db!
     @alice_account = create_and_verify_account!
-    login!
+    login! # Creates a default batch and menusration for account
     @alice_account.mensuration.update(height: "160")
     logout!
   end
@@ -15,14 +15,15 @@ class EntriesActionTest < CapybaraTestCase
     super
   end
 
-  def delete_batches(account_id)
+  def delete_batches
     Batch.each(&:destroy)
-    assert_equal 0, Batch.all.size
+    assert_equal 0, Batch.count
+    assert_equal 0, Entry.count
   end
 
   def around
     # Make sure there is no preexisting batch
-    delete_batches(@alice_account.id)
+    delete_batches
 
     DB.transaction(rollback: :always, savepoint: true, auto_savepoint: true) do
       login!
@@ -32,9 +33,11 @@ class EntriesActionTest < CapybaraTestCase
 
   def test_can_create_entry_without_an_already_existing_batch
     # artificially delete any preexisting batch for testing purpose
-    delete_batches(@alice_account.id)
+    delete_batches
 
-    visit "/entries/new"
+    visit "/entries/new" # This should create a batch named New Batch for account
+    assert_equal 1, Batch.count
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
     assert_current_path "/entries/new"
     assert_content "New Entry"
     assert_content "Day"
@@ -42,42 +45,43 @@ class EntriesActionTest < CapybaraTestCase
     assert_content "Note"
     assert_button "Validate"
 
-    fill_in "Day", with: "30/04/2021"
+    fill_in "Day", with: "01/01/2021"
     fill_in "Weight", with: "52.0"
     fill_in "Note", with: "This is my first test entry"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
+    assert_content "Fri 01 Jan 2021"
     assert_content "52.0"
     assert_content "20.3" # BMI
     # assert_content "This is my first test entry" Cannot test it without JS testing driver enabled
-    assert_equal 1, Batch.where(account_id: @alice_account.id).all.length
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
 
     # Add another entry to test delta
 
     visit "/entries/new"
 
-    fill_in "Day", with: "01/05/2021"
+    fill_in "Day", with: "02/01/2021"
     fill_in "Weight", with: "53.0"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
-    assert_content "Sat 01 May 2021"
+    assert_content "Fri 01 Jan 2021"
+    assert_content "Sat 02 Jan 2021"
     assert_content "52.0"
     assert_content "53.0"
     assert_content "+1.0"
     assert_content "20.3" # First BMI
     assert_content "20.7" # Second BMI
-    assert_equal 1, Batch.where(account_id: @alice_account.id).all.length
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
   end
 
   def test_can_create_entry_with_an_already_existing_active_batch
     # Beacuse of login! action called in the around hook, an active batch has already been created
-    batches = Batch.where(account_id: @alice_account.id).all
-    assert_equal 1, batches.size
-    active_batch = batches.first
+    assert_equal 1, Batch.count
+
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
+    active_batch = Batch.first
 
     visit "/entries/new"
     assert_current_path "/entries/new"
@@ -87,35 +91,35 @@ class EntriesActionTest < CapybaraTestCase
     assert_content "Note"
     assert_button "Validate"
 
-    fill_in "Day", with: "30/04/2021"
+    fill_in "Day", with: "03/01/2021"
     fill_in "Weight", with: "52.0"
     fill_in "Note", with: "This is my first test entry"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
+    assert_content "Sun 03 Jan 2021"
     assert_content "52.0"
     # assert_content "This is my first test entry" Cannot test it without JS testing driver enabled
 
-    entry = Entry.where(day: "2021-04-30", account_id: @alice_account.id).first
+    entry = Entry.where(day: "2021-01-03", account_id: @alice_account.id).first
     assert_equal active_batch.id, entry.batch.id
 
     # Add another entry to test delta
 
     visit "/entries/new"
 
-    fill_in "Day", with: "01/05/2021"
+    fill_in "Day", with: "04/01/2021"
     fill_in "Weight", with: "53.0"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
-    assert_content "Sat 01 May 2021"
+    assert_content "Sun 03 Jan 2021"
+    assert_content "Mon 04 Jan 2021"
     assert_content "52.0"
     assert_content "53.0"
     assert_content "+1.0"
 
-    entry = Entry.where(day: "2021-05-01", account_id: @alice_account.id).first
+    entry = Entry.where(day: "2021-01-04", account_id: @alice_account.id).first
     assert_equal active_batch.id, entry.batch.id
   end
 
@@ -133,35 +137,35 @@ class EntriesActionTest < CapybaraTestCase
     assert_content "Note"
     assert_button "Validate"
 
-    fill_in "Day", with: "30/04/2021"
+    fill_in "Day", with: "05/01/2021"
     fill_in "Weight", with: "52.0"
     fill_in "Note", with: "This is my first test entry"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
+    assert_content "Tue 05 Jan 2021"
     assert_content "52.0"
     # assert_content "This is my first test entry" Cannot test it without JS testing driver enabled
 
-    entry = Entry.where(day: "2021-04-30", account_id: @alice_account.id).first
+    entry = Entry.where(day: "2021-01-05", account_id: @alice_account.id).first
     assert_equal active_batch, entry.batch
 
     # Add another entry to test delta
 
     visit "/entries/new"
 
-    fill_in "Day", with: "01/05/2021"
+    fill_in "Day", with: "06/01/2021"
     fill_in "Weight", with: "53.0"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Fri 30 Apr 2021"
-    assert_content "Sat 01 May 2021"
+    assert_content "Tue 05 Jan 2021"
+    assert_content "Wed 06 Jan 2021"
     assert_content "52.0"
     assert_content "53.0"
     assert_content "+1.0"
 
-    entry = Entry.where(day: "2021-05-01", account_id: @alice_account.id).first
+    entry = Entry.where(day: "2021-01-06", account_id: @alice_account.id).first
     assert_equal active_batch.id, entry.batch.id
 
     assert_equal 0, inactive_batch.reload.entries.length
@@ -170,15 +174,15 @@ class EntriesActionTest < CapybaraTestCase
   def test_cannot_create_entry_with_invalid_params
     # Insert a valid entry first
     visit "/entries/new"
-    fill_in "Day", with: "01/05/2022"
+    fill_in "Day", with: "07/01/2021"
     fill_in "Weight", with: "53.0"
     fill_in "note", with: "Valid note"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Sun 01 May 2022"
+    assert_content "Thu 07 Jan 2021"
 
-    assert_equal 1, @alice_account.entries.count
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
 
     # Invalid Date format
     visit "/entries/new"
@@ -188,10 +192,11 @@ class EntriesActionTest < CapybaraTestCase
 
     # Issue with invalid date format will be catch by the Roda typecast_param plugin
     assert_raises(Roda::RodaPlugins::TypecastParams::Error) { click_on "Validate" }
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
 
     # Try to create an entry with same date
     visit "/entries/new"
-    fill_in "Day", with: "01/05/2022"
+    fill_in "Day", with: "07/01/2021"
     fill_in "Weight", with: "53.0"
     fill_in "note", with: "Valid note"
     click_on "Validate"
@@ -199,10 +204,11 @@ class EntriesActionTest < CapybaraTestCase
     assert_current_path "/entries" # Default path after posting to /entries
     assert_css ".alert-danger"
     assert_content "Can't have two entries for the same day"
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
 
     # Try to create entry with invalid weight
     visit "/entries/new"
-    fill_in "Day", with: "02/05/2022"
+    fill_in "Day", with: "08/01/2021"
     fill_in "Weight", with: "5553.0"
     fill_in "note", with: "Valid note"
     click_on "Validate"
@@ -210,10 +216,11 @@ class EntriesActionTest < CapybaraTestCase
     assert_current_path "/entries"
     assert_css ".alert-danger"
     assert_content "Invalid weight, must be between 20.0 and 999.9"
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
 
     # Try to create entry with too long note
     visit "/entries/new"
-    fill_in "Day", with: "02/05/2022"
+    fill_in "Day", with: "08/01/2021"
     fill_in "Weight", with: "53.0"
     fill_in "note", with: ("Valid note" * 600)
     click_on "Validate"
@@ -221,16 +228,17 @@ class EntriesActionTest < CapybaraTestCase
     assert_current_path "/entries"
     assert_css ".alert-danger"
     assert_content "note is longer than 600 characters"
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
   end
 
   def test_can_create_an_entry_and_attach_it_to_a_non_active_batch
-    assert_equal 1, Batch.where(account_id: @alice_account.id).all.size
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
     @alice_account.add_batch(name: "non active batch", active: false)
-    assert_equal 2, Batch.where(account_id: @alice_account.id).all.size
+    assert_equal 2, Batch.where(account_id: @alice_account.id).count
 
     visit "/entries/new"
     
-    fill_in "Day", with: "01/05/2022"
+    fill_in "Day", with: "08/01/2021"
     fill_in "Weight", with: "53.0"
     fill_in "note", with: "Valid note"
     select("non active batch")
@@ -238,13 +246,13 @@ class EntriesActionTest < CapybaraTestCase
 
     assert_current_path "/entries"
     assert_css ".alert-success"
-    assert_equal 1, @alice_account.entries.count
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
     assert_equal 1, Batch.where(name: "non active batch").first.entries.count
     assert_equal 0, Batch.where(name: "New Batch").first.entries.count
   end
 
   def test_gets_redirected_to_batches_pages_if_many_batches_but_none_active
-    assert_equal 1, Batch.where(account_id: @alice_account.id).all.size
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
     Batch.where(account_id: @alice_account.id).first.update(active: false)
 
     Batch.new(account_id: @alice_account.id, active: false, name: "Inactive Batch").save
@@ -265,86 +273,86 @@ class EntriesActionTest < CapybaraTestCase
   def test_can_update_an_entry_with_valid_params
     visit "/entries/new"
 
-    fill_in "Day", with: "30/04/2021"
+    fill_in "Day", with: "09/01/2021"
     fill_in "Weight", with: "52.0"
     click_on "Validate"
 
     active_batch = Batch.where(account_id: @alice_account.id).first
 
-    click_on "Fri 30 Apr 2021"
+    click_on "Sat 09 Jan 2021"
 
     assert_content "Edit Entry"
-    assert_field "Day", with: "2021-04-30"
+    assert_field "Day", with: "2021-01-09"
     assert_field "Weight", with: "52.0"
     assert_field "Note", with: ""
     assert_button "Validate"
     assert_button "Delete"
 
-    fill_in "Day", with: "01/05/2021"
+    fill_in "Day", with: "10/01/2021"
     fill_in "Weight", with: "54.0"
     fill_in "Note", with: "Updated entry"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Sat 01 May 2021"
+    assert_content "Sun 10 Jan 2021"
     assert_content "54.0"
 
     assert_equal "Updated entry", Entry.where(account_id: @alice_account.id).first.note
-    assert_equal 1, Batch.where(account_id: @alice_account.id).all.length
-    assert_equal 1, Entry.where(account_id: @alice_account.id).all.length
+    assert_equal 1, Batch.where(account_id: @alice_account.id).count
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
     assert_equal active_batch, Entry.where(account_id: @alice_account.id).first.batch
 
     # Transfer entry to another batch
     @alice_account.add_batch(name: "test batch", active: false)
 
-    click_on "Sat 01 May 2021"
+    click_on "Sun 10 Jan 2021"
     assert_content "Edit Entry"
     select "test batch"
     click_on "Validate"
 
-    assert_equal 2, Batch.where(account_id: @alice_account.id).all.length
-    assert_equal 1, Entry.where(account_id: @alice_account.id).all.length
+    assert_equal 2, Batch.where(account_id: @alice_account.id).count
+    assert_equal 1, Entry.where(account_id: @alice_account.id).count
     assert_equal 0, Batch.where(name: "New Batch").first.entries.size
     assert_equal 1, Batch.where(name: "test batch").first.entries.size
     assert_current_path "/entries"
-    refute_content "Sat 01 May 2021"
+    refute_content "Sun 10 Jan 2021"
 
     visit "/batches"
     click_on "test batch"
     assert_current_path "/batches/#{Batch.where(name: "test batch").first.id}/edit"
     click_on "View Entries"
-    assert_content "Sat 01 May 2021"
+    assert_content "Sun 10 Jan 2021"
   end
 
   def test_cannot_update_an_entry_with_invalid_params
     # Insert a valid entry first
     visit "/entries/new"
-    fill_in "Day", with: "01/05/2022"
+    fill_in "Day", with: "11/01/2021"
     fill_in "Weight", with: "53.0"
     fill_in "note", with: "Valid first note"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Sun 01 May 2022"
+    assert_content "Mon 11 Jan 2021"
 
     # Insert a second entry to test entry per day uniqueness
     visit "/entries/new"
-    fill_in "Day", with: "02/05/2022"
+    fill_in "Day", with: "12/01/2021"
     fill_in "Weight", with: "54.0"
     fill_in "note", with: "Valid second note"
     click_on "Validate"
 
     assert_current_path "/entries"
-    assert_content "Mon 02 May 2022"
+    assert_content "Tue 12 Jan 2021"
+
+    assert_equal 2, Entry.where(account_id: @alice_account.id).count
 
     @alice_account.reload
-    assert_equal 2, @alice_account.entries.size
-
     first_test_entry = @alice_account.entries.select { _1.weight == "53.0"}.first
     second_test_entry = @alice_account.entries.select { _1.weight == "54.0"}.first
 
     # Invalid Date format
-    click_on "Sun 01 May 2022"
+    click_on "Mon 11 Jan 2021"
     assert_current_path "/entries/#{first_test_entry.id}/edit"
 
     fill_in "Day", with: "Not A Date"
@@ -356,9 +364,9 @@ class EntriesActionTest < CapybaraTestCase
 
     # Try to update an entry with same date as another entry
     visit "/entries"
-    click_on "Sun 01 May 2022"
+    click_on "Mon 11 Jan 2021"
     assert_current_path "/entries/#{first_test_entry.id}/edit"
-    fill_in "Day", with: "02/05/2022"
+    fill_in "Day", with: "12/01/2021"
     click_on "Validate"
 
     assert_current_path "/entries/#{first_test_entry.id}" # Default path after posting to /entries/id/edit
@@ -367,7 +375,7 @@ class EntriesActionTest < CapybaraTestCase
 
     # Try to update entry with invalid weight
     visit "/entries"
-    click_on "Sun 01 May 2022"
+    click_on "Mon 11 Jan 2021"
     fill_in "Weight", with: "5553.0"
     click_on "Validate"
 
@@ -377,7 +385,7 @@ class EntriesActionTest < CapybaraTestCase
 
     # Try to update entry with too long note
     visit "/entries"
-    click_on "Sun 01 May 2022"
+    click_on "Mon 11 Jan 2021"
     fill_in "note", with: ("Valid note" * 600)
     click_on "Validate"
 
@@ -389,36 +397,20 @@ class EntriesActionTest < CapybaraTestCase
   def test_can_delete_an_entry
     visit "/entries/new"
 
-    fill_in "Day", with: "30/04/2021"
+    fill_in "Day", with: "13/01/2021"
     fill_in "Weight", with: "52.0"
     click_on "Validate"
 
     active_batch = Batch.where(account_id: @alice_account.id).first
 
-    click_on "Fri 30 Apr 2021"
+    click_on "Wed 13 Jan 2021"
 
     click_on "Delete"
 
     assert_current_path "/entries"
-    refute_content "Fri 30 Apr 2021"
+    refute_content "Wed 13 Jan 2021"
     refute_content "52.0"
 
     assert_equal 0, active_batch.entries.length
-  end
-
-  def test_performs_validation_on_weight_before_encryption
-    visit "/entries/new"
-
-    fill_in "Day", with: "30/04/2021"
-    fill_in "Weight", with: "5552.0"
-
-    click_on "Validate"
-
-    assert_current_path "/entries"
-    assert_css ".alert-danger"
-    assert_content "Invalid weight, must be between 20.0 and 999.9"
-    assert_field "Day", with: "2021-04-30"
-    assert_field "Weight", with: "5552.0"
-    assert_field "Note", with: ""
   end
 end
